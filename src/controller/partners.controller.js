db5e828fd600bbe97483aa0817da2819912699a6
Suvra Deb Paul <suvra.deb.paul@gmail.com>
2025-11-10 13:55:06 +0600

    adding dublicate profile prevent with partner email

--- src/controller/partners.controller.js
+++ src/controller/partners.controller.js
@@ -7,11 +7,19 @@ const partnersCollection = db.collection("partners");
 // CREATE PARTNER
 exports.createPartner = async (req, res) => {
   try {
+    const { email } = req.body;
+    const existingPartner = await partnersCollection.findOne({ email });
+    if (existingPartner) {
+      return res.status(400).send({
+        success: false,
+        message: "Partner with this email already exists",
+      });
+    }
     const result = await partnersCollection.insertOne(req.body);
-    //console.log("Inserted Data Successfully");
+    console.log("Inserted Data Successfully");
     res.send(result);
-  } catch (err) {
-    res.status(500).send({ error: err.message });
+  } catch (error) {
+    res.status(500).send({ success: false, error: error.message });
   }
 };
 
